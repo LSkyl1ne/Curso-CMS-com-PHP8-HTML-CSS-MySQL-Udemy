67ba9931497cba73226f819f383c6ec270242d87
LSkyl1ne <lucas1marchesan@gmail.com>
2022-03-24 09:53:05 -0300

    first commit

---
+++ src/Views/commons/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8"/>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+    <title>Base Landing Page</title>
+    <link href="<?=URL_BASE?>resources/css/css.css" rel="stylesheet"/>
+</head>
+<body>
+	<header>
+		header
+	</header>
