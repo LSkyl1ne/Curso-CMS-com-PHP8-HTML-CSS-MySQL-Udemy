2202dc871912419b8958e37a215eb2b709f57b98
LSkyl1ne <lucas1marchesan@gmail.com>
2022-03-28 16:12:53 -0300

    first commit

--- resources/js/js.js
+++ resources/js/js.js
@@ -2,5 +2,6 @@ let menu = document.querySelector(".menu");
 let bar = document.querySelector(".bar");
 
  bar.addEventListener("click", () => {
+    menu.classList.add("active");
     console.log(menu.classList);
 });
