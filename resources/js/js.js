9983b2dfb4e784e78747977f43d2f4964733256c
LSkyl1ne <lucas1marchesan@gmail.com>
2022-03-28 16:50:22 -0300

    first commit

--- resources/js/js.js
+++ resources/js/js.js
@@ -1,11 +1,11 @@
 let menu = document.querySelector(".menu");
 let bar = document.querySelector(".bar");
-let barButton = document.querySelector(".bar-button");
+let barButton = document.querySelectorAll(".bar-button");
 
 bar.addEventListener("click", () => {
     if(menu.classList.value == "menu"){
         menu.classList.add("active");
-        barButton.outerHTML = "";
+        bar.removeChild(barButton)
     }
     else if(menu.classList.value == "menu active"){
          menu.classList.remove("active");
