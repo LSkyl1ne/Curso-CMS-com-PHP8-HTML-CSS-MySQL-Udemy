daa739982eb84850aff6fb565c5a12ba17e0dca7
LSkyl1ne <lucas1marchesan@gmail.com>
2022-03-28 19:30:44 -0300

    first commit

--- header.php
+++ header.php
@@ -48,9 +48,9 @@
                             <path fill-rule="evenodd" d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z"/>
                             <path fill-rule="evenodd" d="M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z"/>
                         </svg>
-                        <div class="bar-button"></div>
-                        <div class="bar-button"></div>
-                        <div class="bar-button"></div>
+                        <div class="bar-button active"></div>
+                        <div class="bar-button active"></div>
+                        <div class="bar-button active"></div>
                     </div>
                     <div class="menu">
                         <ul>
--- resources/js/js.js
+++ resources/js/js.js
@@ -6,15 +6,16 @@ bar.addEventListener("click", () => {
     if(menu.classList.value == "menu"){
         menu.classList.add("active");
         barButton.forEach(button => {
-            barButton.removeChild(barButton)
+            button.classList.remove("active")
         });
-        x.style.display = "block"
+        x.classList.add("active")
     }
     else if(menu.classList.value == "menu active"){
          menu.classList.remove("active");
          x.style.display = ""
          barButton.forEach(button => {
-            button.replaceChild(x)
+            button.classList.add("active")
         });
+        x.classList.remove("remove")
     }
 });
