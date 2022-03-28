db89d3b6bb11f423ff3ce0d681c6716313ab07ed
LSkyl1ne <lucas1marchesan@gmail.com>
2022-03-28 16:36:16 -0300

    first commit

--- resources/js/js.js
+++ resources/js/js.js
@@ -1,13 +1,11 @@
 let menu = document.querySelector(".menu");
 let bar = document.querySelector(".bar");
 
- bar.addEventListener("click", () => {
-     if(menu.classList.value == "menu"){
+bar.addEventListener("click", () => {
+    if(menu.classList.value == "menu"){
         menu.classList.add("active");
-     }
-     else if(menu.classList.value == "menu active"){
+    }
+    else if(menu.classList.value == "menu active"){
          menu.classList.remove("active");
-     }
-    
-    console.log(menu.classList);
+    }
 });
