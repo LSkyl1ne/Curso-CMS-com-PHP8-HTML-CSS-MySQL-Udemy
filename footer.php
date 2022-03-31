cf519d3f02bb4b761a31c132352f2b03b02c3214
LSkyl1ne <lucas1marchesan@gmail.com>
2022-03-30 22:03:42 -0300

    first commit

--- footer.php
+++ footer.php
@@ -11,7 +11,6 @@
                     beatae fuga debitis blanditiis quasi ullam voluptatum!
                     Dolor consequatur eos obcaecati modi quae vitae nulla?
                 </p>
-
             </div>
             <div class="redes-sociais">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-facebook" viewBox="0 0 16 16">
