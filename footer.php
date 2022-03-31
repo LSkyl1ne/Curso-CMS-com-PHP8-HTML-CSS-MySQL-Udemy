538911033df649712f4debda20593b917052b0cf
LSkyl1ne <lucas1marchesan@gmail.com>
2022-03-30 22:27:46 -0300

    first commit

--- footer.php
+++ footer.php
@@ -55,34 +55,30 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
                         <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
                     </svg>
-                    <div class="texto">
+                    <div class="footer-texto">
                         <h4>Endereço</h4>
                         <p>Rua Américo Scaranello, Wilson Bernardi, Sertãozinho-SP</p>
                     </div>
-
                 </div>
                 <div class="telefone">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-phone-fill" viewBox="0 0 16 16">
                         <path d="M3 2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V2zm6 11a1 1 0 1 0-2 0 1 1 0 0 0 2 0z"/>
                     </svg>
-                    <div class="texto">
+                    <div class="footer-texto">
                         <h4>Telefone</h4>
                         <p>(XX) XXXXXX-XXXX</p>
                     </div>
-
                 </div>
                 <div class="email">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-envelope-fill" viewBox="0 0 16 16">
                         <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
                     </svg>
-                    <div class="texto">
+                    <div class="footer-texto">
                         <h4>E-mail</h4>
                         <p>youremail@gmail.com</p>
                     </div>
-
                 </div>
             </div>
-
         </div>
     </div>
     <div class="footer-bottom">
