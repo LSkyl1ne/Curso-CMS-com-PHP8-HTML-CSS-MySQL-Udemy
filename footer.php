ae2c62401b29cb04dde9c1362b43430529d74095
LSkyl1ne <lucas1marchesan@gmail.com>
2022-04-01 16:48:15 -0300

    first commit

--- footer.php
+++ footer.php
@@ -57,7 +57,7 @@
             <div class="titulo"><h3>Contatos</h3></div>
             <div class="contatos">
                 <div class="div-contato endereco">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="25" fill="white" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
                         <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
                     </svg>
                     <div class="footer-texto">
